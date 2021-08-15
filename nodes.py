66e573f66dc1377c8452b0ee35ee8469f1fca350
khlorghaal <khlorghaal@live.com>
2021-08-14 20:39:44 -0400

    violent lambda refucktor

--- nodes.py
+++ nodes.py
@@ -1,92 +1,60 @@
-#pos= lambda tree: [n() for n in tree]
 from typing import Iterable
+from itertools import chain
+from functools import reduce as fold
+import operator
 
-leafy= lambda l: not isinstance(l,Iterable) or isinstance(l,str)
-jl= lambda l:''.join(['%2s'%s for s in l])
-
-nprint= lambda n,d,i: print('%2s %i %i'%(str(n),d,i))
-def tmap(f,t,d=0,i=0):
-	dp= d+1
-	if leafy(t):
-		f(t,d,i)
-	else:
-		for i_,n in enumerate(t):
-			tmap(f,n,dp,i_)
-
-#tr= [[['a'],'b'],'c']
-tr= [0,1,[0,1,2,3],[0],[[0]],[[[0]]],['a','b']*2]
-print('PYPR')
-print(tr)
-print('TRPR\n n d i')
-tmap(nprint,tr)
-#tmap( lambda n: [chr(ord(c)+1) for c in n], tr)
+tr= [0,1,[0,1,2],[3],[[4]],[[[5]]],['a','b']*2, [[['x'],'y'],'z'], ['i',['j',['k']]]]
+print('PYP  %s'%tr)
 
 
-#object stats
-def total_leaf(n,d,i):
-	total_leaf.sum+=1
-total_leaf.sum=0
-tmap(total_leaf,tr)
-print('LCT %3i'%total_leaf.sum)
+class terminal(list):
+	#prevents list descent, lisp quot analogue
+	#used if you want leaf content to be trees
+	def __init__(self,*v):
+		super().__init__(v)
+leafy= lambda l: not isinstance(l,Iterable) or isinstance(l,terminal) or isinstance(l,str)
 
-def total_node(t,d=0,i=0):
-	total_node.sum+= 1
-	if not leafy(t):
-		dp= d+1
-		for i_,n in enumerate(t):
-			total_node(n,dp,i_)
-total_node.sum= 0
-total_node(tr)
-print('NCT %3i'%total_node.sum)
+#leafs and nodes
+#nodes may or may not have data
+#leafs inherently always have data, which may be null; a leaf of an empty set is unpossible
+tmap_leafy= lambda f    ,n,d=0,i=0:  f(n,d,i) if leafy(n) else            [tmap_leafy(f    ,n_,d+1,i_) for i_,n_ in enumerate(n)]
+tmap_leafn= lambda f    ,n,d=0,i=0:        [] if leafy(n) else ( f(n,d,i),[tmap_leafn(    f,n_,d+1,i_) for i_,n_ in enumerate(n)])
+tmap_bisex= lambda fl,fn,n,d=0,i=0: fl(n,d,i) if leafy(n) else (fn(n,d,i),[tmap_bisex(fl,fn,n_,d+1,i_) for i_,n_ in enumerate(n)])
 
-def depths(n,d=0,i=0):
-	if leafy(n):
-		depths.l+=[d]
-	else:
-		dp= d+1
-		for i_,n in enumerate(n):
-			depths(n,dp,i_)
-depths.l= []
-depths(tr)
-print('LDP '+jl(depths.l))
+#empty [] is comparable to something between void and none
+
+tflat= lambda n: [n] if leafy(n) else [ l for i in n for l in tflat(i)]
+
+tsum=    lambda l: fold(operator.add,tflat(l))
+tmax=    lambda l: fold(max,tflat(l))
+strang= lambda n: '%2s'%n if leafy(n) else ['%2s'%s for s in n]
+tstr= lambda nl: lambda l: nl.join(tflat(tmap_leafy(lambda n,d,i: strang(n), l)))
+tstrcat= tstr(' ')
+tstrgrd= tstr('\n')
 
 
-def lengths(n,d=0,i=0):
-	if not leafy(n):
-		lengths.l+=[len(n)]
-		dp= d+1
-		for i_,n in enumerate(n):
-			lengths(n,dp,i_)
-lengths.l= []
-lengths(tr)
-print('LEN '+jl(lengths.l))
+LCT= len(tflat(tr)) #leaf count
+NCT= tsum( tmap_leafn(lambda n,d,i:1 ,tr)) #node count
+ND = tmap_leafn(lambda n,d,i:    d ,tr)#node depth
+NLN= tmap_leafn(lambda n,d,i:len(n),tr)#node lenth
+print('LCT %3i'%LCT)
+print('NCT %3i'%NCT)
+print('ND  '   +tstrcat(ND ) )
+print('NLN '   +tstrcat(NLN) )
 
+
+print('ALL\n N  D  CI| IND')
+info=lambda n,d=0,i=0,x=[]: terminal(n,d,i,x) if leafy(n) else [info(n_,d+1,i_,x+[i_]) for i_,n_ in enumerate(n)]
+ALL= info(tr)
+ALL= map(tstrcat,ALL)
+print(tstrgrd(ALL))
+
+
+
+#W= tmax(tmap_leafy(lambda n,d,i: ?? ,info))
+#H= tmax(NL)
+#print('W:%i H:%i'%(W,H))
 
 #raster
-rast= [[0]*16 for _ in range(8)]
-rpl= 0
-def rput(n,d,i):
-	#rast[0][0]=n
-	global rpl
-	rast[i][d]= n
-	rpl+=1
-tmap(rput,tr)
-sacc= ''
-for l in rast:
-	sacc+= jl(l)
-	sacc+= '\n'
-print('RST')
-print(sacc)
-
-
-#list of index descents per leaf
-def indxs(n,p=[]):
-	if leafy(n):
-		indxs.l+= [p]
-	else:
-		for i,n_ in enumerate(n):
-			indxs(n_,p+[i])
-indxs.l= []
-indxs(tr)
-print('IND')
-print(indxs.l)
+#rast= [terminal([0]*W) for _ in range(H)]
+#print(tstrgrd(rast))
